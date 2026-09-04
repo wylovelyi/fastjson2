65ca93ab0b01b207e5591a00ee7522b2008feb8a
wylovelyi <wylovelyi@users.noreply.github.com>
2026-09-04 09:00:44 +0000

    fix: ClassCastException in ObjectWriterAdapter.toJSONObject when the cached initWriter does not match the runtime value class, for issue #7853

--- core/src/main/moditect/module-info.java
+++ core/src/main/moditect/module-info.java
@@ -2,8 +2,6 @@
 
 open module com.alibaba.fastjson2 {
     requires transitive jdk.unsupported;
-    requires static java.xml;
-    requires static java.logging;
     requires java.management;
 
     exports com.alibaba.fastjson2;
